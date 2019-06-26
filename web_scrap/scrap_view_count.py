10c5339b5fe711b362c0b07e36d96dc3af970434
Taeho Jeon <thjeon.wa@gmail.com>
2019-06-25 17:47:30 -0700

    add scrap number of likes and comments

--- web_scrap/scrap_view_count.py
+++ web_scrap/scrap_view_count.py
@@ -15,19 +15,42 @@ db = connection['youtube_scrap']
 def get_view_count(browser, url):
     """Return the view_count and timestamp"""
     browser.get(url)
-    time.sleep(2 + random.randint(0, 4))
+    time.sleep(1 + random.randint(0, 4))
     now = datetime.now()
+
+    browser.execute_script("window.scrollTo(0, window.scrollY + 640)")
+    time.sleep(4)
+
+    # view-count
     sel = 'span.view-count'
-    view_count = browser.find_element_by_css_selector(sel).text
+    view_count_element = browser.find_element_by_css_selector(sel).text
+    count = ''.join([n for n in view_count_element if n.isdigit()])
+    view_count = int(count) if len(count) > 0 else 0
+    
+    # number of likes
+    like_sel = 'ytd-toggle-button-renderer.style-text[is-icon-button] #text.ytd-toggle-button-renderer'
+    like_count_element = browser.find_element_by_css_selector(like_sel).get_attribute('aria-label')
+    if len(like_count_element) > 0:
+        like = ''.join([n for n in like_count_element if n.isdigit()])
+        like_count = int(like) if len(like) > 0 else 0
+
+    # number of comments
+    class_name = 'count-text'
+    count_text_element = browser.find_element_by_class_name('count-text').text
+    comment = ''.join([n for n in count_text_element if n.isdigit()])
+    comment_count = int(comment) if len(comment) > 0 else 0
 
     return {
-        'view_count': int(''.join([n for n in view_count if n.isdigit()])),
+        'view_count': view_count,
+        'comment_count': comment_count,
+        'like_count': like_count,
         'timestamp': now }
 
 # Get list of videos to collect view counts
 coll = db['videos']
 cur = coll.find({})
 videos = [video for video in cur]
+random.shuffle(videos)
 
 # set browser to headless
 options = Options()
@@ -46,14 +69,17 @@ with Display(visible=1, size=(1280, 800)) as display:
         for i, video in enumerate(videos):
             url = video['url']
             count = get_view_count(browser, url)
-            print(i, ': ', video['title'], ' - ',count['view_count'])
-            
+            print(i, ': ', video['title'], ' - ', url)
+            print(count)
+
             count_coll.insert_one({
                 'title': video['title'],
                 'view_count': count['view_count'],
+                'comment_count': count['comment_count'],
+                'like_count': count['like_count'],
                 'timestamp': count['timestamp']
             })
-        # Idle 5 mins
-        time.sleep(300)
+        # Idle 1+ mins
+        time.sleep(60 + random.randint(0, 60))
 
 browser.close()
