990fcb1d0dad555e38ec9cc8877befad8a46f68e
Taeho Jeon <thjeon.wa@gmail.com>
2019-06-26 18:00:30 -0700

    add sentiment analyze with TextBlob

---
+++ web_scrap/set_sentiment.py
@@ -0,0 +1,21 @@
+from pymongo import MongoClient 
+from datetime import datetime
+
+from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
+
+connection = MongoClient()
+db = connection['youtube_scrap']
+
+coll = db['comments']
+cur = coll.find({ 'sentiment' : { '$exists': False } }).limit(100)
+comments = [comment for comment in cur]
+
+for i, comment in enumerate(comments):
+    sentiment = TextBlob(comment['comment'], analyzer=NaiveBayesAnalyzer()).sentiment
+    db.Doc.update_one({"_id": comment["_id"]}, 
+        {"$set": {"sentiment": sentiment.p_pos}})
+
+    print(datetime.now(), i, comment['comment'][:10], sentiment.p_pos)
+
+    
