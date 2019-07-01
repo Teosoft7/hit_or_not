91c362e4ed9bfb16847d7f10a56c0261a7909ee7
Taeho Jeon <thjeon.wa@gmail.com>
2019-07-01 10:59:40 -0700

    add show video from db on index page

--- web_app/app.py
+++ web_app/app.py
@@ -1,16 +1,26 @@
 from flask import Flask, request, render_template, jsonify, redirect, url_for
+from pymongo import MongoClient
 
 import pickle
 
 # with open('spam_model.pkl', 'rb') as f:
 #     model = pickle.load(f)
 
+connection = MongoClient(port=47017)
+db = connection['youtube_scrap']
+
 app = Flask(__name__, static_url_path="")
+
+def get_last3_videos():
+    video_coll = db['video_detail']
+    cursor = video_coll.find({}).limit(3)
+    return [record for record in cursor]
 
 @app.route('/')
 def index():
     """Return the main page."""
-    return render_template('index.html')
+    last3_video = get_last3_videos()
+    return render_template('index.html', last3_video=last3_video)
 
 @app.route('/about')
 def about():
